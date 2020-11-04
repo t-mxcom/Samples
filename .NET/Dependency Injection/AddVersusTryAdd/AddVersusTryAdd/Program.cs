aa8b9e1e76e5516271037eeb8b814aecbad2e002
Markus Essmayr <markus.essmayr@programmierfabrik.at>
2020-11-04 16:21:06 +0100

    Created sample project

---
+++ .NET/Dependency Injection/AddVersusTryAdd/AddVersusTryAdd/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AddVersusTryAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ServiceCollection serviceCollection = new ServiceCollection();
+            // Add...
+
+            // Mix AddSingleton und AddScoped -> selbes Interface registrieren und schauen, was bei GetService(s) kommt
+
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IDisposable test = serviceProvider.GetService<IDisposable>();
+
+            using (IServiceScope serviceScope = serviceProvider.CreateScope())
+            {
+                // Scoped Services
+            }
+        }
+    }
+}
